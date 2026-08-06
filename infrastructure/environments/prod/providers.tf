19974af36fa7effc4402c4420b1075908eaf8be1
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2026-08-06 14:52:26 +0300

    infra: added providers and variables to each environment for terraform

---
+++ infrastructure/environments/prod/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.57.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "shorts-generator-tfstate-prod-469061454457-us-east-1-an"
+    key            = "terraform.tfstate"
+    use_lockfile   = true
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
