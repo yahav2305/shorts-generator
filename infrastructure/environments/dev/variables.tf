f23c55c270add7b97311e0174b7bf18246e4ed87
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2026-08-06 14:52:26 +0300

    infra: added providers and variables to each environment for terraform

---
+++ infrastructure/environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "org_name" {
+  type        = string
+  description = "The organization name (will be prefixed in resource names)"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region resources are deployed in"
+}
